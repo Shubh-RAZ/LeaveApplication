88454e0b8659fa801d6453a91dd26e426a7c2066
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2024-06-15 17:31:39 +0530

    Add project files.

---
+++ LeaveApplication/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Shubham",
+                DateOfBirth = new DateTime(1999,10,26)
+            };
+            return View(data);
+        }
+    }
+}
